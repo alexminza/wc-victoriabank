33af9675942dd6038f435f26330a64274fa3cffb
Alexander Minza <alexander.minza@gmail.com>
2026-01-05 09:05:25 +0200

    process_response_data TRX_TYPE_AUTHORIZATION message

--- wc-victoriabank.php
+++ wc-victoriabank.php
@@ -1137,7 +1137,7 @@ function victoriabank_plugins_loaded_init()
                         //endregion
 
                         /* translators: 1: Order ID, 2: Payment method title, 3: Payment data */
-                        $message = esc_html(sprintf(__('Order #%1$s payment completed via %2$s: %3$s', 'wc-victoriabank'), $this->get_method_title(), $rrn));
+                        $message = esc_html(sprintf(__('Order #%1$s payment authorized via %2$s: %3$s', 'wc-victoriabank'), $this->get_method_title(), $rrn));
                         $message = $this->get_test_message($message);
                         $this->log(
                             $message,
