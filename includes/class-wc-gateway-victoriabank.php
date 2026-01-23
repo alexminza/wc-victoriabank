a8c5441f07ea27db576c1432ad91dc8b76376b40
Alexander Minza <alexander.minza@gmail.com>
2026-01-23 18:26:23 +0200

    process_refund check_settings wp_strip_all_tags

--- includes/class-wc-gateway-victoriabank.php
+++ includes/class-wc-gateway-victoriabank.php
@@ -1122,16 +1122,10 @@ class WC_Gateway_Victoriabank extends WC_Payment_Gateway_Base
      */
     public function process_refund($order_id, $amount = null, $reason = '')
     {
-        $this->log(
-            __FUNCTION__,
-            \WC_Log_Levels::DEBUG,
-            array(
-                'order_id' => $order_id,
-                'amount' => $amount,
-                'reason' => $reason,
-                'backtrace' => true,
-            )
-        );
+        if (!$this->check_settings()) {
+            $message = wp_strip_all_tags($this->get_settings_admin_message());
+            return new \WP_Error('check_settings', $message);
+        }
 
         $order = wc_get_order($order_id);
         $order_currency = $order->get_currency();
